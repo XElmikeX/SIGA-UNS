0496d1dbd89a3dd27133c3e3c9cbe06f83a45cb5
XElmikeX <urbanodiaze@gmail.com>
2025-12-06 15:19:29 -0500

    Se agrega al Proyecto

--- php/proceso-register.php
+++ php/proceso-register.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // RUTA CORRECTA (asumiendo que estás en una subcarpeta, y yave.php está en la raíz)
-require_once __DIR__ . '/../yave.php'; 
+require_once __DIR__ . '/../yave.php';
 
 // Llamar a conectarDB() explícitamente
 $conexion = conectarDB();
