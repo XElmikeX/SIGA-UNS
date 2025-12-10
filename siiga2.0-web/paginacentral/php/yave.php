6c51399dd0214ca0036157daba595f5b8415053e
XElmikeX <urbanodiaze@gmail.com>
2025-12-09 22:15:44 -0500

    Se agrega al Producto

--- siiga2.0-web/paginacentral/php/yave.php
+++ siiga2.0-web/paginacentral/php/yave.php
@@ -1,24 +1,29 @@
 <?php
-    function conexionDB(){
-        $db_url=getenv('DATABASE_URL');
+    function conectarDB() {
+        $db_url = getenv('DATABASE_URL');
 
-        $db_opts=parse_url($db_url);
-
+        // Parsear la URL:host, puerto, usuario, contraseña, nom del BD
+        $db_opts = parse_url($db_url);
+        
         $host = $db_opts['host'];
-        $port = $db_opts['port'];
-        $db =ltrim($db_opts['path'],'/');
+        $port = $db_opts['port']; // Usará 8080 si está en la URL
+        $db   = ltrim($db_opts['path'], '/');
         $user = $db_opts['user'];
         $pass = $db_opts['pass'];
-
+        
+        // Asegurar conexión SSL o TLS(importante para Railway)
         $dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
-
-        try{
-            $conexion = new PDO([
+        
+        try {
+            $conexion = new PDO($dsn, $user, $pass, [
+                //mejorar manejo de errores"lo envia abajo")
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             ]);
+            
             return $conexion;
-        }catch (PDOException $e){
-            error_log("El error esta" . $e->getMessage());
+        } catch (PDOException $e) {
+            error_log("Error PDO: " . $e->getMessage());
+            error_log("DSN intentado: $dsn");
             return false;
         }
     }
